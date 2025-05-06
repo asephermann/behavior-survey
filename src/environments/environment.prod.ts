f688b1fbea45d9d1b94a6e59018488f96051cd70
Nursalam <35249316+asephermann@users.noreply.github.com>
2025-05-07 05:24:03 +0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCZc_N6-bsoraEYKrQW9y9F3SebmHtQwXI",
+    authDomain: "behavior-survey.firebaseapp.com",
+    projectId: "behavior-survey",
+    storageBucket: "behavior-survey.firebasestorage.app",
+    messagingSenderId: "997337479536",
+    appId: "1:997337479536:web:2e419ae65b98ea406777b5"
+  }
 };
