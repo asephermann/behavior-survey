971b964909ed2c08ae23ee8b1718059747927b11
Asep Herman Nursalam <asephnursalam@gmail.com>
2025-05-10 21:18:39 +0700

    add resize none on keyboard plugin

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'behavior-survey',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    Keyboard: {
+      resize: "none"
+    }
+  }
 };
 
 export default config;
